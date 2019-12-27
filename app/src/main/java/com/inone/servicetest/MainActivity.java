7818d518ebc60cba7954dd5f4c90b3d4c7023c70
Yonghong.Xu <Yonghong.Xu@Lavaszx.com>
2019-12-27 23:49:29 +0800

    第二次提交

--- app/src/main/java/com/inone/servicetest/MainActivity.java
+++ app/src/main/java/com/inone/servicetest/MainActivity.java
@@ -2,14 +2,31 @@ package com.inone.servicetest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ComponentName;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private static final String TAG = "MainActivity";
+    private MyService.DownloadBinder downloadBinder;
+    private ServiceConnection connection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {//活动和服务成功绑定时候调用
+            downloadBinder =(MyService.DownloadBinder) service;
+            downloadBinder.startDownload();
+            downloadBinder.getProgress();
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {//活动和服务断开的时候调用
+
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +35,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         //获取startService，stopService按钮的实例
         Button startService = (Button) findViewById(R.id.start_service);
         Button stopService = (Button) findViewById(R.id.stop_service);
+        Button bindService = (Button) findViewById(R.id.bind_service);
+        Button unbindService = (Button) findViewById(R.id.unbind_service);
+        bindService.setOnClickListener(this);
+        unbindService.setOnClickListener(this);
         startService.setOnClickListener(this);
         stopService.setOnClickListener(this);
     }
@@ -33,6 +54,18 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
                 Intent stopIntent = new Intent(this,MyService.class);
                 stopService(stopIntent);//停止服务
+                break;
+            case R.id.bind_service:
+                Intent bindIntent = new Intent(this,MyService.class);
+                bindService(bindIntent,connection,BIND_AUTO_CREATE);//第一个参数：Intent对象，第二个参数：ServiceConnection的实例。第三个参数：标志位，绑定服务 BIND_AUTO_CREATE自动创建服务
+                break;
+            case R.id.unbind_service:
+             try {
+                 unbindService(connection);//解绑定服务
+             }catch (Exception e){
+                 e.printStackTrace();
+             }
+
                 break;
                 default:
                     break;
--- app/src/main/java/com/inone/servicetest/MyService.java
+++ app/src/main/java/com/inone/servicetest/MyService.java
@@ -1,19 +1,43 @@
 package com.inone.servicetest;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.os.Binder;
 import android.os.IBinder;
 import android.util.Log;
 
+import androidx.core.app.NotificationCompat;
+
 public class MyService extends Service {
     private static final String TAG = "MyService";
+
+    private DownloadBinder mBinder = new DownloadBinder();
+    class DownloadBinder extends Binder {
+
+
+        public void startDownload() {
+            Log.d(TAG, "startDownload: *************");
+        }
+
+        public int getProgress() {
+            Log.d(TAG, "getProgress: ****");
+            return 0;
+        }
+    }
     public MyService() {
     }
 
     @Override
     public IBinder onBind(Intent intent) {
         // TODO: Return the communication channel to the service.
-        throw new UnsupportedOperationException("Not yet implemented");
+//        throw new UnsupportedOperationException("Not yet implemented");
+        return mBinder;
     }
 
 //创建服务时调用第一次创建的时候调用
@@ -21,6 +45,24 @@ public class MyService extends Service {
     public void onCreate() {
         super.onCreate();
         Log.d(TAG, "onCreate: **************************");
+        String id = "com.inone.servicetes";
+        //创建通道
+        NotificationChannel channel = new NotificationChannel(id,"xxx", NotificationManager.IMPORTANCE_LOW);
+        //状态栏通知管理类
+        NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        manager.createNotificationChannel(channel);
+
+        Intent intent = new Intent(this,MainActivity.class);
+        PendingIntent pi  = PendingIntent.getActivity(this,0,intent,0);
+        Notification notification = new NotificationCompat.Builder(this).setChannelId(id)
+                .setContentTitle("这是一个标题")
+                .setContentText("这是文本内容")
+                .setWhen(System.currentTimeMillis())
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setLargeIcon(BitmapFactory.decodeResource(getResources(),R.mipmap.ic_launcher))
+                .setContentIntent(pi)
+                .build();
+        startForeground(1,notification);
         
     }
 
